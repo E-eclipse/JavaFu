df46b9775800e305c0d0fe3936527b339bd04abb
E-eclipse <simernin.mat@gmail.com>
2025-02-04 16:55:01 +0300

    vidgets

---
+++ app/src/main/java/com/example/vidgetspara/WidgetProvider.java
@@ -0,0 +1,30 @@
+package com.example.vidgetspara;
+
+import android.app.PendingIntent;
+import android.app.RemoteAction;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+public class WidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds){
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        for(int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_veiw);
+
+            Intent intent = new Intent(context, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+            views.setOnClickPendingIntent(R.id.widget_button, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+}
